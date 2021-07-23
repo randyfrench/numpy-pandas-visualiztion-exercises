9de97724bf5c2180aedd03bd86360e988d5c4f83
randyfrench <randy.a.french@gmail.com>
2021-07-22 22:43:25 -0500

    exercises completed

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -55,15 +55,23 @@ a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Use python's built in functionality/operators to determine the following:
 # Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
+sum_of_a = sum(a)
+sum_of_a
 
 # Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
+min_of_a = min(a)
+min_of_a
 
 # Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
+max_of_a = max(a)
+max_of_a
 
 # Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
+mean_of_a = sum(a) / len(a)
+mean_of_a
 
 # Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
-
+product_of_a = 
 # Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
 
 # Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
